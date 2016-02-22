147c739d3495ac85379579fd279325c9c538826c
monsterb <unix.s3c@gmail.com>
2016-02-22 01:58:29 -0600

    comments

---
+++ groundhog.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# author:      monsterb (https://www.monsterb.org)
+# discription: groundhog is an information gathering tool for pentesters.
+# email:       unix.s3c (at) gmail (dot) com
+# filename:    groundhog.py
+# version:     0.00
